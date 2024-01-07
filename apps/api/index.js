c7b65683ca3bf3a0311b0e32ed0d06dccc11ec8b
abhilasha <abhilashaduwal@gmail.com>
2024-01-07 14:16:27 +0545

    fix: change the default database

--- apps/api/index.js
+++ apps/api/index.js
@@ -26,8 +26,7 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?
-authSource=admin`;
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/mydb?authSource=admin`;
 
 const connectWithRetry = () => {
   mongoose
